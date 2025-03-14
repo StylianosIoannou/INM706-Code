ee5e53d7eedb5e8e8a081acfcb3d7010d6077294
Stylianos Ioannou <stylianos.ioannou@city.ac.uk>
2025-03-14 13:57:07 +0000

    wanbd Setup

--- train.py
+++ train.py
@@ -1,1 +1,33 @@
 #training script of the model, also where wandb logs the metrics
+
+import random
+
+import wandb
+
+# Start a new wandb run to track this script.
+run = wandb.init(
+    # Set the wandb entity where your project will be logged (generally your team name).
+    entity="INM706",
+    # Set the wandb project where this run will be logged.
+    project="INM706",
+    # Track hyperparameters and run metadata.
+    config={
+        "learning_rate": 0.02,
+        "architecture": "CNN",
+        "dataset": "CIFAR-100",
+        "epochs": 10,
+    },
+)
+
+# Simulate training.
+epochs = 10
+offset = random.random() / 5
+for epoch in range(2, epochs):
+    acc = 1 - 2**-epoch - random.random() / epoch - offset
+    loss = 2**-epoch + random.random() / epoch + offset
+
+    # Log metrics to wandb.
+    run.log({"acc": acc, "loss": loss})
+
+# Finish the run and upload any remaining data.
+run.finish()
