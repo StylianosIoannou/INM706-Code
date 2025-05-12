f598517b940a3faf3ab7d5b2a3aed4e4fe1af4ec
danny121145 <98962440+danny121145@users.noreply.github.com>
2025-05-13 00:49:28 +0100

    removed unenecsary file and added content to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
 #shell script to create a virtual environment and install dependencies
+#!/bin/bash
+# Create virtual environment
+python -m venv venv
+# Activate environment
+source venv/bin/activate
+# Upgrade pip
+pip install --upgrade pip
+# Install required packages
+pip install -r requirements.txt
